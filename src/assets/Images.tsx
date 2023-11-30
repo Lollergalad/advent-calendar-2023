be54415fdea7de50c3c727893fd9c0cc1f6a9170
Norbert Urbán <norinho89@gmail.com>
2023-12-01 00:17:48 +0100

    11/change_images_logic

--- src/assets/Images.tsx
+++ src/assets/Images.tsx
@@ -1,37 +1,7 @@
 const baseURL = import.meta.url;
 
-const DAY_ICONS = {
-  day1: new URL("./day-1.png", baseURL).href,
-  day2: new URL("./day-2.png", baseURL).href,
-  day3: new URL("./day-3.png", baseURL).href,
-  day4: new URL("./day-4.png", baseURL).href,
-  day5: new URL("./day-5.png", baseURL).href,
-  day6: new URL("./day-6.png", baseURL).href,
-  day7: new URL("./day-7.png", baseURL).href,
-  day8: new URL("./day-8.png", baseURL).href,
-  day9: new URL("./day-9.png", baseURL).href,
-  day10: new URL("./day-10.png", baseURL).href,
-  day11: new URL("./day-11.png", baseURL).href,
-  day12: new URL("./day-12.png", baseURL).href,
-  day13: new URL("./day-13.png", baseURL).href,
-  day14: new URL("./day-14.png", baseURL).href,
-  day15: new URL("./day-15.png", baseURL).href,
-  day16: new URL("./day-16.png", baseURL).href,
-  day17: new URL("./day-17.png", baseURL).href,
-  day18: new URL("./day-18.png", baseURL).href,
-  day19: new URL("./day-19.png", baseURL).href,
-  day20: new URL("./day-20.png", baseURL).href,
-  day21: new URL("./day-21.png", baseURL).href,
-  day22: new URL("./day-22.png", baseURL).href,
-  day23: new URL("./day-23.png", baseURL).href,
-  day24: new URL("./day-24.png", baseURL).href,
-  christmasPresent: new URL("./christmas_present.png", baseURL).href,
-  christmasTree: new URL("./christmas_tree.png", baseURL).href,
-  icy: new URL("./icy.jpg", baseURL).href,
-};
-
 const IMAGES = {
   calendarLogo: new URL("./calendar-logo.png", baseURL).href,
 };
 
-export { DAY_ICONS, IMAGES };
+export { IMAGES };
--- src/views/calendar/Calendar.tsx
+++ src/views/calendar/Calendar.tsx
@@ -2,7 +2,31 @@ import { FC, useState } from "react";
 import Snowfall from "react-snowfall";
 import classNames from "classnames";
 
-import { DAY_ICONS } from "assets/Images";
+import day1 from "assets/day-1.png";
+import day2 from "assets/day-2.png";
+import day3 from "assets/day-3.png";
+import day4 from "assets/day-4.png";
+import day5 from "assets/day-5.png";
+import day6 from "assets/day-6.png";
+import day7 from "assets/day-7.png";
+import day8 from "assets/day-8.png";
+import day9 from "assets/day-9.png";
+import day10 from "assets/day-10.png";
+import day11 from "assets/day-11.png";
+import day12 from "assets/day-12.png";
+import day13 from "assets/day-13.png";
+import day14 from "assets/day-14.png";
+import day15 from "assets/day-15.png";
+import day16 from "assets/day-16.png";
+import day17 from "assets/day-17.png";
+import day18 from "assets/day-18.png";
+import day19 from "assets/day-19.png";
+import day20 from "assets/day-20.png";
+import day21 from "assets/day-21.png";
+import day22 from "assets/day-22.png";
+import day23 from "assets/day-23.png";
+import day24 from "assets/day-24.png";
+import icy from "assets/icy.jpg";
 
 import classes from "./Calendar.module.scss";
 
@@ -30,80 +54,80 @@ const getImage = (day: number) => {
   let image;
   switch (day) {
     case 1:
-      image = DAY_ICONS.day1;
+      image = day1;
       break;
     case 2:
-      image = DAY_ICONS.day2;
+      image = day2;
       break;
     case 3:
-      image = DAY_ICONS.day3;
+      image = day3;
       break;
     case 4:
-      image = DAY_ICONS.day4;
+      image = day4;
       break;
     case 5:
-      image = DAY_ICONS.day5;
+      image = day5;
       break;
     case 6:
-      image = DAY_ICONS.day6;
+      image = day6;
       break;
     case 7:
-      image = DAY_ICONS.day7;
+      image = day7;
       break;
     case 8:
-      image = DAY_ICONS.day8;
+      image = day8;
       break;
     case 9:
-      image = DAY_ICONS.day9;
+      image = day9;
       break;
     case 10:
-      image = DAY_ICONS.day10;
+      image = day10;
       break;
     case 11:
-      image = DAY_ICONS.day11;
+      image = day11;
       break;
     case 12:
-      image = DAY_ICONS.day12;
+      image = day12;
       break;
     case 13:
-      image = DAY_ICONS.day13;
+      image = day13;
       break;
     case 14:
-      image = DAY_ICONS.day14;
+      image = day14;
       break;
     case 15:
-      image = DAY_ICONS.day15;
+      image = day15;
       break;
     case 16:
-      image = DAY_ICONS.day16;
+      image = day16;
       break;
     case 17:
-      image = DAY_ICONS.day17;
+      image = day17;
       break;
     case 18:
-      image = DAY_ICONS.day18;
+      image = day18;
       break;
     case 19:
-      image = DAY_ICONS.day19;
+      image = day19;
       break;
     case 20:
-      image = DAY_ICONS.day20;
+      image = day20;
       break;
     case 21:
-      image = DAY_ICONS.day21;
+      image = day21;
       break;
     case 22:
-      image = DAY_ICONS.day22;
+      image = day22;
       break;
     case 23:
-      image = DAY_ICONS.day23;
+      image = day23;
       break;
     case 24:
-      image = DAY_ICONS.day24;
+      image = day24;
       break;
 
     default:
-      image = DAY_ICONS.day1;
+      image = day1;
       break;
   }
 
@@ -153,7 +177,7 @@ const Calendar: FC<CounterProps> = ({
                 }>
                 <img src={img} alt={day.toString()} />
                 {day > timeLeftUntilCalendarOpens.days + 1 ? (
-                  <img className={classes.dis} src={DAY_ICONS.icy} alt={day.toString()} />
+                  <img className={classes.dis} src={icy} alt={day.toString()} />
                 ) : null}
               </div>
             </div>
